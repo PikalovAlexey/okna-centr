ae5b6a91d0695dcb5563db8f8810168766aa0b92
Alexey <altovskit@gmail.com>
2022-05-23 22:00:12 +0300

    added function get object with resoult quiz version 1.0

--- js/test.js
+++ js/test.js
@@ -1,9 +1,24 @@
-function make_couner(){
-    let count = 0
-    return function() {
-        return count++
-    }
-}
+function get_obj_resoult_quiz() {
+    let quiz_block = document.getElementsByClassName('frame__quiz')
+    let arr_resoult = {}
+    let ask = []
+    let answer_inputs = []
 
-let counter = make_couner()
-console.log(counter()+ counter())
+    for(i=0; i < quiz_block.length; i++){
+        ask[i] = quiz_block[i].getElementsByClassName('ask')[0].innerHTML;
+        answer_inputs[i] = quiz_block[i].getElementsByTagName('input')
+        
+        for (j=0; j < answer_inputs[i].length; j++) {
+            if(answer_inputs[i][j].checked) {
+                arr_resoult[ask[i]] = answer_inputs[i][j].getAttribute('id')
+                break
+            }
+            arr_resoult[ask[i]] = null
+            
+        }
+        
+    }
+    console.log(arr_resoult)
+    console.log(ask)
+}
+get_obj_resoult_quiz()
