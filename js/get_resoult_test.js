3805a4ed0f718451b24644ef80b3aa6dbe3a4144
Alexey <altovskit@gmail.com>
2022-05-24 13:30:32 +0300

    little changes

--- js/get_resoult_test.js
+++ js/get_resoult_test.js
@@ -8,7 +8,7 @@ function get_obj_resoult_quiz() {
         ask[i] = quiz_block[i].getElementsByClassName('ask')[0].innerHTML;
         answer_inputs_radio[i] = quiz_block[i].getElementsByTagName('input')
         
-        for (j=0; j < answer_inputs[i].length; j++) {
+        for (j=0; j < answer_inputs_radio[i].length; j++) {
             if(answer_inputs_radio[i][j].checked) {
                 arr_resoult[ask[i]] = answer_inputs_radio[i][j].getAttribute('id')
                 break
@@ -16,6 +16,7 @@ function get_obj_resoult_quiz() {
             arr_resoult[ask[i]] = null
         }
     }
+    console.log(arr_resoult)
 }
 
 
